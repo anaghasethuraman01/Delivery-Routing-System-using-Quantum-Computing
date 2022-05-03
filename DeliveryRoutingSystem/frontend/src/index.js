182ccf6a436968e20538d48951dd10b1825e321d
Komal Joshi <komal.joshi@sjsu.edu>
2022-05-02 23:58:29 -0700

    reverted index js changes

--- DeliveryRoutingSystem/frontend/src/index.js
+++ DeliveryRoutingSystem/frontend/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+
 //render App component on the root element
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
