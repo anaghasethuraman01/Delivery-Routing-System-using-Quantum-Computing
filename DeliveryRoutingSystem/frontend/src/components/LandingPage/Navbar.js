3174890e77e98d1394f7b14ed455800e7a290184
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2022-09-05 23:11:40 -0700

    navbar fix

--- DeliveryRoutingSystem/frontend/src/components/LandingPage/Navbar.js
+++ DeliveryRoutingSystem/frontend/src/components/LandingPage/Navbar.js
@@ -53,6 +53,7 @@ class Navbar extends Component {
 		this.setState({ show: false });
 	};
 	render() {
+		var redirectVar = <Redirect to="/login" />;
 		var modalview = null;
 		if (
 			this.state.show &&
@@ -78,7 +79,7 @@ class Navbar extends Component {
 				</Modal>
 			);
 		}
-		redirectVar = <Redirect to="/login" />;
+
 		const userSession = localStorage.getItem("pageFind");
 		let sessionAvail = null;
 		if (userSession == "admin") {
@@ -138,7 +139,7 @@ class Navbar extends Component {
 				</div>
 			);
 		}
-		var redirectVar = null;
+
 		return (
 			<div>
 				{redirectVar}
